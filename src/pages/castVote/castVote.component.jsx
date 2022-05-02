9f961e96ecd382428193961ed44c70f77b3ca607
Sammy <sammyoziegbe@gmail.com>
2022-05-02 07:39:26 +0100

    updates

--- src/pages/castVote/castVote.component.jsx
+++ src/pages/castVote/castVote.component.jsx
@@ -1,7 +1,10 @@
 import { Grid } from '@mui/material'
 import { RadioButton } from '../../components/forms/forms.component'
 import { VoteCard } from '../../components/voteCard/voteCard.component'
+import { Button } from '../../components/button/button.component'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import './castVote.styles.css'
+import { Link } from 'react-router-dom';
 
 
 function CastVote() {
@@ -15,6 +18,20 @@ function CastVote() {
 
                 <RadioButton selected />
                 <RadioButton />
+
+                <Grid item container xs={14} justifyContent="center">
+                    <Grid item xs={6}>
+                        <Button className="castVote__button">Submit your vote</Button>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <Link to="/results">
+                            <div className="castVote__jump-to-results">
+                                <span>Jump to results</span> <ChevronRightIcon />
+                            </div>
+                        </Link>
+                    </Grid>
+                </Grid>
             </Grid>
 
             <Grid item xs={2} md={5} />
